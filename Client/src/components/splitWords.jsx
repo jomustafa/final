01461f5e3a72d780dc98fe73777d7f48bd679a23
aymeri <aymeri@citycollege.sheffield.com>
2020-11-13 10:41:56 +0100

    again

--- Client/src/components/splitWords.jsx
+++ Client/src/components/splitWords.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "https://brainbright.herokuapp.com";
+const ENDPOINT = "brainbright.herokuapp.com";
 
 export default function ClientComponent() {
   const [splitWords, setSplitWords] = useState([]);
