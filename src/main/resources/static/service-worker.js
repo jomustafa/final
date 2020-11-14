1be137a253d7a09bfd0f389f5c233f0bfbc3c8fc
vlere <vlere@starlabspro.com>
2020-11-14 14:22:15 +0100

    add build

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbee05439441c34aaadbced80e092495.js"
+  "/precache-manifest.301e150917d479d9a2c3b73909c6b07e.js"
 );
 
 self.addEventListener('message', (event) => {
