30c075dbc85a85055a5b770b6fa7b4a18b8e135b
aymeri <aymeri@citycollege.sheffield.com>
2021-01-11 11:09:31 +0100

    find next image controller

--- src/main/java/com/example/messagingstompwebsocket/games/visual/findNextImage/FindNextController.java
+++ src/main/java/com/example/messagingstompwebsocket/games/visual/findNextImage/FindNextController.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
 
-
+@Controller
 public class FindNextController {
 	FindNext fn;
 	
 	@MessageMapping("/fn_getimages")
 	@SendToUser("/topic/fn_imagelist")
 	public ArrayList<String> getImages(int level) { 
-				
+		System.out.println("Request received");
 		fn = new FindNext(level);
 		ArrayList<String> picNames = new ArrayList<String>();
 		for(Picture_findnext p : fn.getListOfPictureFindnexts()) {
