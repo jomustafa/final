3715d64c963796a9bed49d7a14421b3224bbfc4e
aymeri <aymeri@citycollege.sheffield.com>
2021-01-17 17:30:39 +0100

    early version of pattern game

---
+++ src/main/java/com/example/messagingstompwebsocket/games/photographic/findpatterns/FindPatternsController.java
@@ -0,0 +1,19 @@
+package com.example.messagingstompwebsocket.games.photographic.findpatterns;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FindPatternsController {
+
+	FindPatterns fp;
+	
+	
+	@MessageMapping("/getpatterns")
+	@SendToUser("/topic/patternlist")
+	public Pattern[] getPatterns(int level) {
+		fp = new FindPatterns(level);
+		return fp.getPatterns();
+	}
+}
---
+++ src/main/java/com/example/messagingstompwebsocket/games/photographic/findpatterns/Pattern.java
@@ -0,0 +1,46 @@
+package com.example.messagingstompwebsocket.games.photographic.findpatterns;
+
+
+public class Pattern {
+
+
+    protected int size;
+    protected int [] [] pattern;
+
+
+    public Pattern(int size, int numPatterns) {
+        this.size = size;
+        pattern = new int [numPatterns][2];
+        randomizePattern(numPatterns);
+    }
+
+    protected void randomizePattern(int numPatterns) {
+        int [] [] a = new int [size*size][2];
+
+        for(int i = 0; i<size; i++) {
+            for(int j=0; j<size; j++) {
+                a[j+i*size][0]= i;
+                a[j+i*size][1]= j;
+            }
+        }
+
+        for(int i=0; i<size*size; i++) {
+            int random = (int)(Math.random()*size*size);
+            int [] temp = a [random];
+            a[random] = a[i];
+            a[i] = temp;
+        }
+
+        for(int i=0; i<numPatterns; i++) {
+            pattern[i] = a[i];
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int[][] getPattern() {
+        return pattern;
+    }
+}
