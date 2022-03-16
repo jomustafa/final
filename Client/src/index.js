17d3400536101e3ca843e6b64c4e06e7a9a605ab
jomustafa <56961272+jomustafa@users.noreply.github.com>
2022-03-16 19:51:07 +0200

    changes

--- Client/src/index.js
+++ Client/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
+App.use(express.static(__dirname + "/docs"));
 
 ReactDOM.render(
   <App/>,
